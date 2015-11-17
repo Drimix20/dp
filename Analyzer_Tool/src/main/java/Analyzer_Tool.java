d053fe89e864b2846c3d71bce4e1db93ad2f8867
Martin Drimal <373769@mail.muni.cz>
2015-11-17 18:51:17 +0100

    Analyzer_Tool - base module

---
+++ Analyzer_Tool/src/main/java/Analyzer_Tool.java
@@ -0,0 +1,64 @@
+
+import analyzer.tool.gui.InteractiveAnalyzerDialog;
+import ij.IJ;
+import ij.ImageJ;
+import ij.ImagePlus;
+import ij.Prefs;
+import ij.WindowManager;
+import ij.plugin.PlugIn;
+import javax.swing.SwingUtilities;
+
+/**
+ * This main class serve to run plugin in Fiji menu
+ * @author Drimal
+ */
+public class Analyzer_Tool implements PlugIn {
+
+    @Override
+    public void run(String arg) {
+
+        SwingUtilities.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                preparationForDevelop();
+
+                InteractiveAnalyzerDialog dialog = new InteractiveAnalyzerDialog();
+                dialog.setLocationRelativeTo(null);
+                dialog.setVisible(true);
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        // set the plugins.dir property to make the plugin appear in the Plugins menu
+        Class<?> clazz = Analyzer_Tool.class;
+        String url = clazz.getResource("/" + clazz.getName().replace('.', '/') + ".class").toString();
+        String pluginsDir = url.substring(5, url.length() - clazz.getName().length() - 6);
+        System.setProperty("plugins.dir", pluginsDir);
+
+        // start ImageJ
+        new ImageJ();
+
+        // run the plugin
+        IJ.runPlugIn(clazz.getName(), "");
+    }
+
+    private static void preparationForDevelop() {
+        //Import jpk image
+        ImagePlus imp = IJ.openVirtual("C:\\Users\\Drimal\\Downloads\\zasilka-CHKRI8DLZPAYS4EY\\thyroglobulin 669 kDa-2013.08.13-13.59.14_height_trace.jpk");
+        //Convert stack slices into separated images
+        IJ.run(imp, "Stack to Images", "");
+        //Retrieve height image
+        ImagePlus heightImg = WindowManager.getImage("thyroglobulin-0002");
+        //Threshold height image by triangle method
+        IJ.setAutoThreshold(heightImg, "Triangle");
+        Prefs.blackBackground = false;
+        IJ.run(heightImg, "Convert to Mask", "");
+        //Compute default measurements and show results table and roi manager
+        IJ.run(heightImg, "Analyze Particles...", "display add");
+        //Compute default measurements and show just results
+        //IJ.run(heightImg, "Analyze Particles...", "display");
+    }
+
+}
